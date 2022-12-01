9f1eccf79aae829139afd4fec0ebcbc8045782f2
Kwame Sarfo <kwame.sarfo@turntabl.io>
2022-12-01 10:42:33 +0000

    day1 refactoring

--- src/main/java/Day1/Calories.java
+++ src/main/java/Day1/Calories.java
@@ -1,16 +1,12 @@
 package Day1;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Calories {
 
