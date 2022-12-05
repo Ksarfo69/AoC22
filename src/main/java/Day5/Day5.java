36f28da7bdf9f0f87e8992252f2f1e56148f235f
ksarfo69 <ksarfo69@gmail.com>
2022-12-05 06:35:48 +0000

    working hash

--- src/main/java/Day5/Day5.java
+++ src/main/java/Day5/Day5.java
@@ -10,7 +10,7 @@ public class Day5 {
         List<String> lines = Files.readAllLines(Path.of("src/main/resources/stack-input.txt"));
 
 
-        Map<Integer, String> stack = new HashMap<>();
+        Map<Integer, List> stack = new HashMap<>();
 
 
         int iter = 0;
@@ -18,45 +18,54 @@ public class Day5 {
 
 
         //8
-        while(iter < 3)
-        {
-            stack.put(hashIndex, lines.get(iter));
-            hashIndex+=1;
-            iter+=1;
+            for(int i = 2; i>=0; i--) {
+                String line = lines.get(i);
+
+                String[] splittedLine = line.split(" ");
+
+                for (int j = 0; j< splittedLine.length; j++)
+                {
+                    if(!stack.containsKey(j+1))
+                    {
+                        stack.put(j+1, new ArrayList<>());
+                        stack.get(j+1).add(splittedLine[j]);
+                    }
+                    else {
+                        stack.get(j+1).add(splittedLine[j]);
+                    }
+                }
         }
 
         System.out.println(stack);
 
         //10
-        for(int i = 5; i<lines.size(); i++)
-        {
-            String line = lines.get(i);
-
-            String[] splittedLine = line.split(" ");
-
-            int moveQuantity = Integer.valueOf(splittedLine[1]);
-
-            int origin = Integer.valueOf(splittedLine[3]);
-
-            int destination = Integer.valueOf(splittedLine[5]);
-
-
-            int moveIterator = 0;
-
-            List<String> originStack = new ArrayList<>();
-            List<String> destinationStack = new ArrayList<>();
-
-            for(String row : stack.values())
-            {
-                String[] splittedRow = row.split(" ");
-
-                System.out.println(Arrays.toString(splittedRow));
-                originStack.add(splittedRow[origin-1]);
-                destinationStack.add(splittedRow[destination-1]);
-                return;
-            }
-
-            System.out.println(originStack);
+//        for(int i = 5; i<lines.size(); i++)
+//        {
+//            String line = lines.get(i);
+//
+//            String[] splittedLine = line.split(" ");
+//
+//            int moveQuantity = Integer.valueOf(splittedLine[1]);
+//
+//            int origin = Integer.valueOf(splittedLine[3]);
+//
+//            int destination = Integer.valueOf(splittedLine[5]);
+//
+//
+//            int moveIterator = 0;
+//
+//            List<String> originStack = new ArrayList<>();
+//            List<String> destinationStack = new ArrayList<>();
+//
+//            for(String row : stack.values())
+//            {
+//                String[] splittedRow = row.split(" ");
+//
+//                originStack.add(splittedRow[origin-1]);
+//                destinationStack.add(splittedRow[destination-1]);
+//            }
+//
+//            System.out.println(originStack);
 
 //            List<String> destinationStack = new ArrayList<>();
 //
@@ -67,7 +76,7 @@ public class Day5 {
 //                originStack.add(splittedRow[destination]);
 //
 //            }
-        }
+  //      }
 
     }
 }
